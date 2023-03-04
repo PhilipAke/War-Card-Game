e23d509954b0d0a256fa071942237344356a7c17
PhilipAke <123983832+PhilipAke@users.noreply.github.com>
2023-03-03 21:32:23 -0600

    Create warchallengeApp.swift

---
+++ warchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warchallengeApp.swift
+//  warchallenge
+//
+//  Created by philip ake on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct warchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
